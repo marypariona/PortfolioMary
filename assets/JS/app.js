40915c0cb740babe894be052087f641e9d2ca7de
maryluz1028 <parionamaryluz@gmail.com>
2022-06-20 11:18:34 -0500

    delete js hero

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -1,8 +1,8 @@
 import hamburger_menu from "./components/hamburger_menu.js";
 import header_background from "./components/header_background.js";
-import nse from "./components/home.js";
+//import nse from "./components/home.js";
 document.addEventListener('DOMContentLoaded',(e)=>{
     hamburger_menu('#hamburger','menu');
     header_background('header');
-    nse('.home');
+    //nse('.home');
 });
