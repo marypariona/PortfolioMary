855f398d9bc90f0533dcdd776739aec3f9d3f59e
maryluz1028 <parionamaryluz@gmail.com>
2022-06-20 18:56:22 -0500

    add animation skills

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -1,8 +1,10 @@
 import hamburger_menu from "./components/hamburger_menu.js";
 import header_background from "./components/header_background.js";
-//import nse from "./components/home.js";
+import noseee from "./components/home.js";
+import nse from "./components/home.js";
 document.addEventListener('DOMContentLoaded',(e)=>{
     hamburger_menu('#hamburger','menu');
     header_background('header');
     //nse('.home');
+    noseee('.progress-bar','.skills');
 });
--- assets/JS/components/home.js
+++ assets/JS/components/home.js
@@ -1,7 +1,27 @@
-export default function nse(hola){
+/*export default function nse(hola){
     var $hero = document.querySelector(hola),
     h = window.innerHeight; 
     console.log($hero);
     $hero.style.height=h+"px";
     
+}*/
+ /*
+    const $s=document.querySelectorAll(skills);
+    $s.forEach(el => {
+        const $data=el.getAttribute('data-percent');
+        el.textContent=$data;
+        const ndn=$data+"% !important";
+        el.setAttribute('style','width:'+ndn);
+    })*/
+export default function  noseee(skills,section){
+    window.addEventListener('scroll',(e)=>{
+        const $section=document.querySelector(section),
+        $position=$section.getBoundingClientRect().top-(innerHeight/2);
+        if(screenY>$position){
+            const $skills=document.querySelectorAll(skills);
+            $skills.forEach(el => {
+                el.classList.add('animar');
+            });
+        }
+    })
 }
